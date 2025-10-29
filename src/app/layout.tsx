d25504d32e4ab94255ee9d1d50da508e880ad893
Dimas Yuda Pratama <dimstillalone@gmail.com>
2025-10-29 14:41:26 +0000

    aku ingin ubah favicon aplikasi ini, aku sudah meletakkan gambar dengan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
   title: 'Schediku',
   description:
     'Lacak pengeluaran, buat anggaran, dan dapatkan kejelasan finansial dengan Schediku.',
+  icons: {
+    icon: '/images/logo.png',
+  },
 };
 
 export default function RootLayout({
@@ -28,7 +31,6 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="shortcut icon" href="images/logo.png" type="image/x-icon" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
